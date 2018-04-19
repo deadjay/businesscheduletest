b9a22247b9a84d4554be8b8a1ccd2b43cfc840ca
Artem <iaffector@gmail.com>
2018-04-19 22:51:47 +0300

    initial commit

---
+++ BusinessCheduleTest/BusinessCheduleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessCheduleTest
+//
+//  Created by Artem Alekseev on 19/04/2018.
+//  Copyright © 2018 Artem Alekseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
