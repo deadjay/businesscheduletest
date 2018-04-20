b05ee6b49537be0893af19adf136040ad65b4d8f
Artem <iaffector@gmail.com>
2018-04-20 22:59:14 +0300

    remove MainStoryboard. Add window instantiation instead. Disable all orientations, except portrait.

--- BusinessCheduleTest/AppDelegate.swift
+++ BusinessCheduleTest/AppDelegate.swift
@@ -14,8 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {	
+		let navigationControler = UINavigationController(rootViewController: BusinessScheduleViewController())
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window!.rootViewController = navigationControler
+		window!.makeKeyAndVisible()
+		
 		return true
 	}
 
